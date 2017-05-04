18ba5c9600743039a5839d7f2eb101b30864df18
preawbnp <budnampetch.o@ku.th>
2017-05-04 23:47:25 +0700

    change directory of map images

--- change-map.js
+++ change-map.js
@@ -1,28 +1,28 @@
 // -------- Change Map Images ----------
 function changeMap24() {
-  $("#way-bg").attr("src","24.png");
+  $("#way-bg").attr("src","./images/bus/24.png");
 }
 
 function changeMap29() {
-  $("#way-bg").attr("src","29.png");
+  $("#way-bg").attr("src","./images/bus/29.png");
 }
 
 function changeMap39() {
-  $("#way-bg").attr("src","39.png");
+  $("#way-bg").attr("src","./images/bus/39.png");
 }
 
 function changeMap59() {
-  $("#way-bg").attr("src","59.png");
+  $("#way-bg").attr("src","./images/bus/59.png");
 }
 
 function changeMap104() {
-  $("#way-bg").attr("src","104.png");
+  $("#way-bg").attr("src","./images/bus/104.png");
 }
 
 function changeMap510() {
-  $("#way-bg").attr("src","510.png");
+  $("#way-bg").attr("src","./images/bus/510.png");
 }
 
 function changeMapAll() {
-  $("#way-bg").attr("src","allbuses.png");
+  $("#way-bg").attr("src","./images/bus/allbuses.png");
 }
