a9cbb1268ae78bf2b372769072db097f0072e2c9
preawbnp <budnampetch.o@ku.th>
2017-05-04 23:18:47 +0700

    already changed bus line with javascript

---
+++ change-map.js
@@ -0,0 +1,28 @@
+// -------- Change Map Images ----------
+function changeMap24() {
+  $("#way-bg").attr("src","24.png");
+}
+
+function changeMap29() {
+  $("#way-bg").attr("src","29.png");
+}
+
+function changeMap39() {
+  $("#way-bg").attr("src","39.png");
+}
+
+function changeMap59() {
+  $("#way-bg").attr("src","59.png");
+}
+
+function changeMap104() {
+  $("#way-bg").attr("src","104.png");
+}
+
+function changeMap510() {
+  $("#way-bg").attr("src","510.png");
+}
+
+function changeMapAll() {
+  $("#way-bg").attr("src","allbuses.png");
+}
