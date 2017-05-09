1a2ee32722db5915b3fb43b44e35c0ae4f4aa120
Piromsurang <Piromsurang@Piromsurangs-MacBook-Pro.local>
2017-05-09 17:15:08 +0900

    fixed getStaffList.php

--- getStaffList.php
+++ getStaffList.php
@@ -9,6 +9,19 @@
 
   $staff = "SELECT * FROM Staff";
   $result = mysqli_query($connection, $showStaff);
-  echo json_encode($result);
+  $table = "";
+  $table .= "<table>";
+  $table .= "<tr><th>StaffID</th>";
+  $table .= "<th>FirstName</th>";
+  $table .= "<th>LastName</th>";
+  $table .= "<th>Gender</th>";
+  $table .= "<th>Age</th>";
+  $table .= "<th>StaffType</th></tr>";
+    while( $row = mysqli_fetch_array($result) ) {
+    $table .= "<tr><td>". $row[0] . "</td><td>" . $row[1] . "</td><td>". $row[2] . "</td><td>". $row[3]
+            . "</td><td>". $row[4] . "</td><td>". $row[5] . "</td></tr>";
+  }
+  $table .= "</table>";
+  echo $table;
   disconnect($connection);
  ?>
