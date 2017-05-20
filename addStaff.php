0badbf06cf54c7760ae37bb3c01e4b48eb35335f
Thanawan <monika-amp@hotmail.com>
2017-05-20 16:29:22 +0700

    amp finish

---
+++ addStaff.php
@@ -0,0 +1,21 @@
+
+
+
+
+<?php
+  include 'connection.php';
+  $firstname = $_POST['firstname'];
+  $lastname = $_POST['lastname'];
+  $gender = $_POST['gender'];
+  $type = $_POST['type'];
+  $age = intval($_POST['age']);
+  $connection = connect();
+  $previouslist_sql = "SELECT * FROM Staff";
+  $result = mysqli_query($connection, $previouslist_sql);
+  $num = mysqli_num_rows($result) + 1;
+  $id = "S$num";
+  $sql = "INSERT INTO `Staff`(`StaffID`, `FirstName`, `LastName`, `Gender`, `Age`, `StaffType`)
+  VALUES ('$id','$firstname','$lastname','$gender','$age','$type')";
+  $result = mysqli_query($connection, $sql);
+  disconnect($connection);
+?>
